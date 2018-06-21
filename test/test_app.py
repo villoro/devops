a4779d96b25cb10def825d70a85f014d493faef6
BTISAVB\arnau.villoro <villoro7@gmail.com>
2018-06-21 22:44:22 +0200

    [REF] Tests inside a folder + small change to try CI/CD

--- test/test_app.py
+++ test/test_app.py
@@ -5,7 +5,7 @@ from src import index
 class Test_app(unittest.TestCase):
 
     def test_values(self):
-        self.assertEqual(index.VAUES, ['LA', 'NYC', 'MTL', 'BCN'])
+        self.assertEqual(index.VAUES, ['MTL', 'BCN'])
 
     def test_server(self):
         self.assertIsNotNone(index.SERVER)
